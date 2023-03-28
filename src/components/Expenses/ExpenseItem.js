a432d509c12f493fc279cf34e66890d711bf5ffd
Anempooja <anempooja@email.com>
2023-03-28 19:23:06 +0530

    change title button added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,19 @@
+import React, {useState} from 'react'
 import './ExpenseItem.css'
 import Card from '../UI/Card'
 import ExpenseDate from './ExpenseDate'
 import ExpenseDetails from './ExpenseDetails'
+
 const ExpenseItem =(props)=>{  
-    
-    const deleteExpense=()=>{
-            
+    const [title,setTitle]=useState(props.title)
+    const clickHandler=()=>{
+          setTitle('updated')  
     }  
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}></ExpenseDate>      
-            <ExpenseDetails title={props.title} amount={props.amount} locationOfExpenditure={props.locationOfExpenditure}></ExpenseDetails>
-            <button id={`${props.id}`} onClick={deleteExpense}></button>
+            <ExpenseDetails title={title} amount={props.amount} locationOfExpenditure={props.locationOfExpenditure}></ExpenseDetails>
+            <button  onClick={clickHandler}>change title</button>
         
         </Card>
     )
