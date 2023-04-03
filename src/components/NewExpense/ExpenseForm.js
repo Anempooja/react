cb7a2b4bcaedfcde8409ac0b069a60faff6a378a
Anempooja <anempooja@email.com>
2023-04-03 16:33:39 +0530

    stored the elements in var

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './ExpenseForm.css'
  function ExpenseForm(){
+    const [enteredTitle,setEnteredTiTle]=useState('')
     const titleChangeHandler=(event)=>{
-        console.log(event.target.value)
+        setEnteredTiTSle(event.target.value)
+    }
+    const [enteredAmount,setEnteredAmount]=useState('')
+    const amountChangeHandler=(event)=>{
+        setEnteredAmount(event.target.value)
+    }
+    const [enteredDate,setEnteredDate]=useState('')
+    const dateChangeHandler=(event)=>{
+        setEnteredDate(event.target.value)
     }
     return <form>
         <div className='new-expense__controls'>
@@ -12,11 +21,11 @@ import './ExpenseForm.css'
             </div>
             <div className='new-empense__control'>
                 <label>Amount</label>
-                <input type='number' min='0.01' step='0.01'/>
+                <input type='number' min='0.01' step='0.01'onChange={amountChangeHandler}/>
             </div>
             <div className='new-empense__control'>
                 <label>date</label>
-                <input type='date' min='2023-04-01' max='2025-01-01'/>
+                <input type='date' min='2023-04-01' max='2025-01-01'onChange={dateChangeHandler}/>
             </div>
         </div>
         <div className='new-expense__actions'>
